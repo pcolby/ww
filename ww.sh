4b0e00028d82fa2ddd99826c99931b5281220fe1
Paul Colby <git@colby.id.au>
2024-02-02 19:27:28 +1100

    Enable all ShellCheck warnings

--- ww.sh
+++ ww.sh
@@ -18,9 +18,9 @@ function showUsage {
 # Parse the CLI's postional arguments.
 if [[ $# -eq 1 ]]; then
   read -r owner repo runId attemptNumber < <(
-    sed -Ene 's|.*/([^/]*)/([^/]*)/actions/runs/([0-9]+)(/attempts/([0-9]+))?$|\1 \2 \3 \5|p' <<< "$1"
+    sed -Ene 's|.*/([^/]*)/([^/]*)/actions/runs/([0-9]+)(/attempts/([0-9]+))?$|\1 \2 \3 \5|p' <<< "$1" || :
   ) || { echo "Invalid workflow run URL: $1" >&2; showUsage; exit 2; }
-  [[ -n "$attemptNumber" ]] || unset attemptNumber
+  [[ -n "${attemptNumber}" ]] || unset attemptNumber
 elif [[ $# -ge 3 && $# -le 4 ]]; then
   owner=$1; repo=$2; runId=$3
   [[ $# -lt 4 ]] || attemptNumber=$4
@@ -28,19 +28,19 @@ else
   showUsage; exit 1
 fi
 
-readonly API_PATH="/repos/$owner/$repo/actions/runs/$runId${attemptNumber:+/attempts/$attemptNumber}"
+readonly API_PATH="/repos/${owner}/${repo}/actions/runs/${runId}${attemptNumber:+/attempts/${attemptNumber}}"
 
 # Generate Mermaid Gantt chart header.
-jq -er --arg displayMode "$DISPLAY_MODE" "$(cat <<-"-"
+jq -er --arg displayMode "${DISPLAY_MODE}" "$(cat <<-"-" || :
         def safe(s): s|gsub("[;#]";""); # Note, `:` is ok in titles.
 	"---\ndisplayMode: " + $displayMode + "\n---\ngantt\n" +
 	"  title " + safe(.name) + " (run " + (.id|tostring) + ", attempt " + (.run_attempt|tostring) + ")\n" +
 	"  dateFormat YYYY-MM-DDTHH:MM:SS.SSSZ\n  %% "+ .html_url
 	-
-	)" < <(gh api "$API_PATH")
+	)" < <(gh api "${API_PATH}" || :)
 
 # Generate Mermaid Gantt chart sections.
-jq -er --argjson minStepDuration "$MIN_STEP_DURATION" "$(cat <<-"-"
+jq -er --argjson minStepDuration "${MIN_STEP_DURATION}" "$(cat <<-"-" || :
 	def isodate(d): d|strptime("%FT%T.000%z")|mktime;
         def isodiff(d1;d2): isodate(d2)-isodate(d1);
         def safe(s): s|gsub("[:;#]";"");
@@ -53,4 +53,4 @@ jq -er --argjson minStepDuration "$MIN_STEP_DURATION" "$(cat <<-"-"
 	  .started_at + ", " + (.duration|tostring) + "s"
 	]|join("\n"))
 	-
-	)" < <(gh api "$API_PATH/jobs" --paginate)
+	)" < <(gh api "${API_PATH}/jobs" --paginate || :)
